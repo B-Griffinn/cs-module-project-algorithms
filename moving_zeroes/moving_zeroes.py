1e59031bac4fe950e946bd3a6c11726846501ef7
Ben <bigriffin94@outlook.com>
2020-05-20 18:03:21 -0700

    Moving zeros iteratively! passing tests OK bigO = O(n^2)

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,28 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    # loop thru the arr
+    for i in range(0, len(arr)):
+        # print(arr[i])
+        # loop thru same arr starting at index i + 1
 
-    pass
+        for x in range(i + 1, len(arr)):
+            if arr[i] == 0 and arr[x] is not None:
+                print('arr', arr)
+                arr[i], arr[x] = arr[x], arr[i]
+
+    return arr
 
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+print(moving_zeroes([0, 3, 1, 0, -2]))
 
-    print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+
+# if __name__ == '__main__':
+#     # Use the main function here to test out your implementation
+#     arr = [0, 3, 1, 0, -2]
+
+#     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
